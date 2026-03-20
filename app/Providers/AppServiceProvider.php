2df8d318bbb79db2bea838fe9df51b17fe99adc4
Ryan Bilesky <1036407+x7ryan@users.noreply.github.com>
2026-03-19 21:30:59 -0700

    AppServiceProvider customizations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,38 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Date::use(
+            CarbonImmutable::class
+        );
+
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+
+        Model::shouldBeStrict(
+            ! $this->app->isProduction()
+        );
+
+        Password::defaults(
+            fn () => $this->app->isProduction()
+                ? Password::min(12)->uncompromised()
+                : Password::min(8)
+        );
+
+        URL::forceHttps();
+    }
 }
